8eb45e8eb42d19a1030f408de4b46ec009235d83
drewhort <83997495+drewhort@users.noreply.github.com>
2024-08-12 12:38:22 -0600

    Update main.py
    wrong file previously upoaded

--- src/main.py
+++ src/main.py
@@ -4,32 +4,38 @@ import pandas as pd
 import sys
 import os
 
-#which model to run options are KPL, PMED, PCENT, KPCON
-MEASURE='KPCON'
+
 
 #define parameters and file paths    
     
 FILE_PATH = '/home/drew/data/'
 CITY_NUM = int(sys.argv[1])
-KP_THRESHOLD=float(sys.argv[2])
+NUM_TO_OPEN=int(sys.argv[2])
+#which model to run options are KPL, PMED, PCENT, KPCON
+MEASURE=str(sys.argv[3])
+AVERSION_PARAMETER=-1
 
-#Computed using data from all 500 cities and AVERSION_PARAMETER=-1
-KAPPA = -0.0003973603173152847
 
 #choice of solver
 SOLVER = "gurobi"
+#SOLVER = pyo.SolverFactory('scip', executable="./scipoptsuite-8.0.4/build/bin/scip") 
+
 
 #import  and clean the data based on user defined parameter    
 ORIGINS_DF,DESTINATIONS_DF,DISTANCES_DF = efl.import_data_files(FILE_PATH,CITY_NUM)
-TOTAL_POP,OPEN_CURRENT_LIST,ORIGINS_LIST,DESTINATIONS_LIST,ORIGINS_DF,DESTINATIONS_DF,DISTANCES_DF,ORIGINS_DICT,DISTANCES_DICT,OPEN_TOTAL = efl.clean_and_format_data(ORIGINS_DF,DESTINATIONS_DF,DISTANCES_DF)
+TOTAL_POP,OPEN_CURRENT_LIST,ORIGINS_LIST,DESTINATIONS_LIST,ORIGINS_DF,DESTINATIONS_DF,DISTANCES_DF,ORIGINS_DICT,DISTANCES_DICT,OPEN_TOTAL = efl.clean_and_format_data(ORIGINS_DF,DESTINATIONS_DF,DISTANCES_DF,NUM_TO_OPEN)
 
 
+#get initial kappa value based on the existing facilities
+KAPPA = efl.compute_kappa(OPEN_CURRENT_LIST,ORIGINS_DF,DISTANCES_DF,AVERSION_PARAMETER)
 #transform distances based on kappa for use in optimization model
 DISTANCES_TRANSFORMED_DICT = efl.transform_distances(DISTANCES_DF,KAPPA)
 
 
-MODEL = efl.create_pyomo_model(KAPPA,ORIGINS_LIST,DESTINATIONS_LIST,ORIGINS_DICT,DISTANCES_DICT,DISTANCES_TRANSFORMED_DICT,OPEN_CURRENT_LIST,OPEN_TOTAL,TOTAL_POP,MEASURE,KP_THRESHOLD)
-RESULTS,MODEL,WALL_TIME,TIME,TERMINATION_CONDITION = efl.solve_model(MODEL,SOLVER)
+MODEL = efl.create_pyomo_model(KAPPA,ORIGINS_LIST,DESTINATIONS_LIST,ORIGINS_DICT,DISTANCES_DICT,DISTANCES_TRANSFORMED_DICT,OPEN_CURRENT_LIST,OPEN_TOTAL,TOTAL_POP,MEASURE)
+RESULTS,MODEL,WALL_TIME,TIME, TERMINATION_CONDITION= efl.solve_model(MODEL,SOLVER)
+NEW_FACILITIES = efl.get_new_facilities(MODEL,DESTINATIONS_LIST)
+
 
 if (TERMINATION_CONDITION == pyo.TerminationCondition.optimal):
     
@@ -54,8 +60,6 @@ else:
     NUM_NEW_FACILITIES = 'n/a'
     
     
-ADD = pd.DataFrame([[CITY_NUM,KP_THRESHOLD,SOLVER,NUM_NEW_FACILITIES,NEW_FACILITIES,KAPPA,KAPPA_OPT,KP_VALUE,PMED_VALUE,PCENT_VALUE,TIME,WALL_TIME,TERMINATION_CONDITION]], columns=['city','KP threshold', 'solver', 'number of new facilities','new facilities','initial kappa','optimal kappa','kp value','average distance','maximum value','time','wall time','termination condition'])
+ADD = pd.DataFrame([[CITY_NUM,MEASURE,SOLVER,NUM_NEW_FACILITIES,NEW_FACILITIES,KAPPA,KAPPA_OPT,KP_VALUE,PMED_VALUE,PCENT_VALUE,TIME,WALL_TIME]], columns=['city','measure', 'solver', 'number of new facilities','new facilities','initial kappa','optimal kappa','kp value','average distance','maximum value','time','wall time'])
 OUTPUT_PATH= str(MEASURE)+'.csv'
 ADD.to_csv(OUTPUT_PATH, mode='a', header=not os.path.exists(OUTPUT_PATH))
-    
-
